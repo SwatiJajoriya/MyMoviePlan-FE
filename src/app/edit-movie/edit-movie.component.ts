99d392912438f6ba484c6a81d8e77c738e5f7ba2
SwatiJajoriya <112231469+SwatiJajoriya@users.noreply.github.com>
2023-04-06 13:30:03 +0530

    Update edit-movie.component.ts

--- src/app/edit-movie/edit-movie.component.ts
+++ src/app/edit-movie/edit-movie.component.ts
@@ -11,37 +11,36 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditMovieComponent implements OnInit {
 
-   data:any;
-  movie:any;
-  id:any;
-  genrez:any;
-  languages:any;
-  submitted:boolean=false;
+  data:any;
+ movie:any;
+ id:any;
+ genrez:any;
+ languages:any;
+ submitted:boolean=false;
 
-  constructor(private service:ServiceService,private router:Router,private route:ActivatedRoute) { }
+ constructor(private service:ServiceService,private router:Router,private route:ActivatedRoute) { }
 
-  ngOnInit(): void {
+ ngOnInit(): void {
 
-    this.id=this.route.snapshot.params['id'];
-    this.service.getMovieById(Number(this.id)).subscribe(data=>this.movie=data);
+   this.id=this.route.snapshot.params['id'];
+   this.service.getMovieById(Number(this.id)).subscribe(data=>this.movie=data);
 
-    let response=this.service.getAllGenre();
-      response.subscribe((data:any)=>this.genrez=data);
-    let response2=this.service.getAllLanguage();
-      response2.subscribe((data:any)=>this.languages=data);
+   let response=this.service.getAllGenre();
+     response.subscribe((data:any)=>this.genrez=data);
+   let response2=this.service.getAllLanguage();
+     response2.subscribe((data:any)=>this.languages=data);
 
-   
   
-    }
-  
+ 
+   }
+ 
 
-    onSubmit(){
-      this.submitted=true;
-      this.service.updateMovie(this.id,this.movie)
-       
-       alert("Data Updated Successfully");
-       this.router.navigate(['./admin-dashboard'])
-      
-      }
-  }
-
+   onSubmit(){
+     this.submitted=true;
+     let response=this.service.updateMovie(this.id,this.movie)
+     response.subscribe((data:any)=>this.movie=data);
+      alert("Data Updated Successfully");
+      this.router.navigate(['./admin-dashboard'])
+     
+     }
+ }
